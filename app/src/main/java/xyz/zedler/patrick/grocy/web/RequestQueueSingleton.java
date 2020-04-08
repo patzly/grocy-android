3281e29f6d6147932549d099c0e20672f861e62b
Patrick Zedler <patrick@zedler.xyz>
2020-04-08 03:17:31 +0200

    Implement RequestQueue in StockFragment

---
+++ app/src/main/java/xyz/zedler/patrick/grocy/web/RequestQueueSingleton.java
@@ -0,0 +1,31 @@
+package xyz.zedler.patrick.grocy.web;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class RequestQueueSingleton {
+    private static RequestQueueSingleton instance;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+
+    private RequestQueueSingleton(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized RequestQueueSingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new RequestQueueSingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+}
---
+++ app/src/main/java/xyz/zedler/patrick/grocy/web/WebRequest.java
@@ -0,0 +1,68 @@
+package xyz.zedler.patrick.grocy.web;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+
+public class WebRequest {
+
+    private RequestQueue requestQueue;
+
+    public WebRequest(RequestQueue requestQueue) {
+        this.requestQueue = requestQueue;
+    }
+
+    public void get(
+            String url,
+            Runnable onQueued,
+            OnResponseListener onSuccess,
+            OnErrorListener onError,
+            Runnable onLeave
+    ) {
+        requestQueue.add(
+                new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        response -> {
+                            onSuccess.onResponse(response);
+                            onLeave.run();
+                        }, error -> {
+                    onError.onError(error.getMessage());
+                })
+        );
+        onQueued.run();
+    }
+
+    public void get(String url, OnResponseListener onSuccess, OnErrorListener onError) {
+        requestQueue.add(
+                new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        response -> {
+                            onSuccess.onResponse(response);
+                        }, error -> {
+                    onError.onError(error.getMessage());
+                })
+        );
+    }
+
+    public void post(String url, String json) {
+
+    }
+
+    public void put(String url, String json) {
+
+    }
+    public void delete(String url, Runnable onQueued) {
+
+    }
+
+    public interface OnResponseListener {
+        void onResponse(String response);
+    }
+
+    public interface OnErrorListener {
+        void onError(String msg);
+        // json format
+    }
+}
