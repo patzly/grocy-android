8a56f3ff2d513d6476937ab9f8cad2f1e0f9567b
Patrick Zedler <patrick@zedler.xyz>
2020-04-17 23:47:51 +0200

    Improve custom chip InputChip with public close method and padding setter in dp

--- app/src/main/java/xyz/zedler/patrick/grocy/view/InputChip.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/view/InputChip.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.os.Handler;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -29,6 +30,7 @@ public class InputChip extends LinearLayout {
     private FrameLayout frameLayoutContainer;
     private View viewClose;
     private TextView textView;
+    private Runnable runnableOnClose;
     private int width;
 
     public InputChip(@NonNull Context context) {
@@ -66,6 +68,7 @@ public class InputChip extends LinearLayout {
         imageViewIcon = findViewById(R.id.image_input_chip_icon);
         textView = findViewById(R.id.text_input_chip);
         viewClose = findViewById(R.id.view_input_chip_close);
+        runnableOnClose = onClose;
 
         if(animate) {
             frameLayoutContainer.setAlpha(0);
@@ -76,44 +79,7 @@ public class InputChip extends LinearLayout {
         setText(text);
 
         viewClose.setOnClickListener(v -> {
-            // DURATIONS
-            int fade = 200, disappear = 300;
-            // run action
-            if(onClose!= null) new Handler().postDelayed(onClose, fade + disappear);
-            // first fade out
-            frameLayoutContainer.animate().alpha(0).setDuration(fade).start();
-            // then make shape disappear
-            width = frameLayoutContainer.getWidth();
-            // animate height
-            ValueAnimator animatorHeight = ValueAnimator.ofInt(frameLayoutContainer.getHeight(), 0);
-            animatorHeight.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    removeAllViews();
-                    if(getParent() != null) {
-                        ((ViewGroup) getParent()).removeView(linearLayoutThis);
-                    }
-                }
-            });
-            animatorHeight.addUpdateListener(
-                    animation -> {
-                        frameLayoutContainer.setLayoutParams(
-                                new LayoutParams(width, (int) animation.getAnimatedValue())
-                        );
-                        frameLayoutContainer.invalidate();
-                    }
-            );
-            animatorHeight.setInterpolator(new FastOutSlowInInterpolator());
-            animatorHeight.setStartDelay(fade);
-            animatorHeight.setDuration(disappear).start();
-            // animate width
-            ValueAnimator animatorWidth = ValueAnimator.ofInt(frameLayoutContainer.getWidth(), 0);
-            animatorWidth.addUpdateListener(
-                    animation -> width = (int) animation.getAnimatedValue()
-            );
-            animatorWidth.setInterpolator(new FastOutSlowInInterpolator());
-            animatorWidth.setStartDelay(fade);
-            animatorWidth.setDuration(disappear).start();
+            close();
         });
     }
 
@@ -131,11 +97,62 @@ public class InputChip extends LinearLayout {
 
     @Override
     public void setPadding(int left, int top, int right, int bottom) {
-        frameLayoutContainer.setPadding(left, top, right, bottom);
+        frameLayoutContainer.setPadding(dp(left), dp(top), dp(right), dp(bottom));
+    }
+
+    public void close() {
+        // DURATIONS
+        int fade = 200, disappear = 300;
+        // run action
+        if(runnableOnClose != null) {
+            new Handler().postDelayed(runnableOnClose, fade + disappear);
+        }
+        // first fade out
+        frameLayoutContainer.animate().alpha(0).setDuration(fade).start();
+        // then make shape disappear
+        width = frameLayoutContainer.getWidth();
+        // animate height
+        ValueAnimator animatorHeight = ValueAnimator.ofInt(frameLayoutContainer.getHeight(), 0);
+        animatorHeight.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                removeAllViews();
+                if(getParent() != null) {
+                    ((ViewGroup) getParent()).removeView(linearLayoutThis);
+                }
+            }
+        });
+        animatorHeight.addUpdateListener(
+                animation -> {
+                    frameLayoutContainer.setLayoutParams(
+                            new LayoutParams(width, (int) animation.getAnimatedValue())
+                    );
+                    frameLayoutContainer.invalidate();
+                }
+        );
+        animatorHeight.setInterpolator(new FastOutSlowInInterpolator());
+        animatorHeight.setStartDelay(fade);
+        animatorHeight.setDuration(disappear).start();
+        // animate width
+        ValueAnimator animatorWidth = ValueAnimator.ofInt(frameLayoutContainer.getWidth(), 0);
+        animatorWidth.addUpdateListener(
+                animation -> width = (int) animation.getAnimatedValue()
+        );
+        animatorWidth.setInterpolator(new FastOutSlowInInterpolator());
+        animatorWidth.setStartDelay(fade);
+        animatorWidth.setDuration(disappear).start();
     }
 
     public void change(String text) {
         textView.setText(text);
         // TODO: animate changes
+    }
+
+    private int dp(float dp){
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp,
+                getResources().getDisplayMetrics()
+        );
     }
 }
