6b4b50ca2755148c789f7d04ba9179a5137998d7
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2020-05-09 16:31:43 +0200

    Amount error only printed if product is selected

--- app/src/main/java/xyz/zedler/patrick/grocy/fragment/ConsumeFragment.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/fragment/ConsumeFragment.java
@@ -189,6 +189,7 @@ public class ConsumeFragment extends Fragment {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
             public void afterTextChanged(Editable s) {
+                if(productDetails == null) return;
                 String input = s.toString();
                 if(!input.equals("")) {
                     amount = Double.parseDouble(input);
