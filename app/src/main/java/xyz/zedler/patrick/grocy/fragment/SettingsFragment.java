d92305db857db77171c8135ed40f295fd6312029
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2020-09-13 15:29:59 +0200

    Create SettingsFragment

---
+++ app/src/main/java/xyz/zedler/patrick/grocy/fragment/SettingsFragment.java
@@ -0,0 +1,64 @@
+package xyz.zedler.patrick.grocy.fragment;
+
+/*
+    This file is part of Grocy Android.
+
+    Grocy Android is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Grocy Android is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grocy Android.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2020 by Patrick Zedler & Dominic Zedler
+*/
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import xyz.zedler.patrick.grocy.activity.MainActivity;
+import xyz.zedler.patrick.grocy.databinding.FragmentSettingsBinding;
+import xyz.zedler.patrick.grocy.util.ClickUtil;
+
+public class SettingsFragment extends BaseFragment {
+
+    private final static String TAG = SettingsFragment.class.getSimpleName();
+
+    private FragmentSettingsBinding binding;
+    private MainActivity activity;
+    private final ClickUtil clickUtil = new ClickUtil();
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        activity = (MainActivity) requireActivity();
+
+        binding.frameAboutBack.setOnClickListener(v -> activity.navigateUp());
+    }
+}
