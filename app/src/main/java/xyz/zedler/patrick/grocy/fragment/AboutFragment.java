8dca1890cae0ce4aaa2204f3d81194ce8ac47428
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2020-12-31 15:21:13 +0100

    Improve AboutFragment

--- app/src/main/java/xyz/zedler/patrick/grocy/fragment/AboutFragment.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/fragment/AboutFragment.java
@@ -56,10 +56,6 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
             Bundle savedInstanceState
     ) {
         binding = FragmentAboutBinding.inflate(inflater, container, false);
-
-        if(binding.translators.getText().toString().trim().isEmpty()) {
-            binding.linearTranslators.setVisibility(View.GONE);
-        }
         return binding.getRoot();
     }
 
@@ -72,10 +68,10 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
     @Override
     public void onViewCreated(@Nullable View view, @Nullable Bundle savedInstanceState) {
         activity = (MainActivity) requireActivity();
-
-        binding.frameAboutBack.setOnClickListener(v -> activity.navigateUp());
+        binding.setActivity(activity);
 
         setOnClickListeners(
+                view,
                 R.id.linear_intro,
                 R.id.linear_changelog,
                 R.id.linear_developers,
@@ -102,9 +98,9 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
         }
     }
 
-    private void setOnClickListeners(@IdRes int... viewIds) {
+    private void setOnClickListeners(View view, @IdRes int... viewIds) {
         for (int viewId : viewIds) {
-            activity.findViewById(viewId).setOnClickListener(this);
+            view.findViewById(viewId).setOnClickListener(this);
         }
     }
 
@@ -112,86 +108,68 @@ public class AboutFragment extends BaseFragment implements View.OnClickListener 
     public void onClick(View v) {
         if(clickUtil.isDisabled()) return;
 
-        switch(v.getId()) {
-            case R.id.linear_intro:
-                IconUtil.start(activity, R.id.image_intro);
-                new Handler().postDelayed(
-                        () -> navigate(AboutFragmentDirections
-                                .actionAboutFragmentToFeaturesFragment()),
-                        150
-                );
-                break;
-            case R.id.linear_changelog:
-                IconUtil.start(activity, R.id.image_changelog);
-                showTextBottomSheet("CHANGELOG", R.string.info_changelog, 0);
-                break;
-            case R.id.linear_developers:
-                IconUtil.start(activity, R.id.image_developers);
-                new Handler().postDelayed(
-                        () -> startActivity(
-                                new Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse(getString(R.string.url_developer))
-                                )
-                        ), 300
-                );
-                break;
-            case R.id.linear_github:
-                startActivity(
-                        new Intent(
-                                Intent.ACTION_VIEW,
-                                Uri.parse(getString(R.string.url_github))
-                        )
-                );
-                break;
-            case R.id.linear_license_material_components:
-                IconUtil.start(activity, R.id.image_license_material_components);
-                showTextBottomSheet(
-                        "APACHE",
-                        R.string.license_material_components,
-                        R.string.url_material_components
-                );
-                break;
-            case R.id.linear_license_material_icons:
-                IconUtil.start(activity, R.id.image_license_material_icons);
-                showTextBottomSheet(
-                        "APACHE",
-                        R.string.license_material_icons,
-                        R.string.url_material_icons
-                );
-                break;
-            case R.id.linear_license_roboto:
-                IconUtil.start(activity, R.id.image_license_roboto);
-                showTextBottomSheet(
-                        "APACHE",
-                        R.string.license_roboto,
-                        R.string.url_roboto
-                );
-                break;
-            case R.id.linear_license_volley:
-                IconUtil.start(activity, R.id.image_license_volley);
-                showTextBottomSheet(
-                        "APACHE",
-                        R.string.license_volley,
-                        R.string.url_volley
-                );
-                break;
-            case R.id.linear_license_gson:
-                IconUtil.start(activity, R.id.image_license_gson);
-                showTextBottomSheet(
-                        "APACHE",
-                        R.string.license_gson,
-                        R.string.url_gson
-                );
-                break;
-            case R.id.linear_license_xzing_android:
-                IconUtil.start(activity, R.id.image_license_xzing_android);
-                showTextBottomSheet(
-                        "APACHE",
-                        R.string.license_xzing_android,
-                        R.string.url_zxing_android
-                );
-                break;
+        if(v.getId() == R.id.linear_intro) {
+            IconUtil.start(activity, R.id.image_intro);
+            new Handler().postDelayed(
+                    () -> navigate(AboutFragmentDirections
+                            .actionAboutFragmentToFeaturesFragment()),
+                    150
+            );
+        } else if(v.getId() == R.id.linear_changelog) {
+            IconUtil.start(activity, R.id.image_changelog);
+            showTextBottomSheet("CHANGELOG", R.string.info_changelog, 0);
+        } else if(v.getId() == R.id.linear_developers) {
+            IconUtil.start(activity, R.id.image_developers);
+            new Handler().postDelayed(
+                    () -> startActivity(
+                            new Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse(getString(R.string.url_developer))
+                            )
+                    ), 300
+            );
+        } else if(v.getId() == R.id.linear_github) {
+            startActivity(
+                    new Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse(getString(R.string.url_github))
+                    )
+            );
+        } else if(v.getId() == R.id.linear_license_material_components) {
+            IconUtil.start(activity, R.id.image_license_material_components);
+            showTextBottomSheet(
+                    "APACHE",
+                    R.string.license_material_components,
+                    R.string.url_material_components
+            );
+        } else if(v.getId() == R.id.linear_license_roboto) {
+            IconUtil.start(activity, R.id.image_license_roboto);
+            showTextBottomSheet(
+                    "APACHE",
+                    R.string.license_roboto,
+                    R.string.url_roboto
+            );
+        } else if(v.getId() == R.id.linear_license_volley) {
+            IconUtil.start(activity, R.id.image_license_volley);
+            showTextBottomSheet(
+                    "APACHE",
+                    R.string.license_volley,
+                    R.string.url_volley
+            );
+        } else if(v.getId() == R.id.linear_license_gson) {
+            IconUtil.start(activity, R.id.image_license_gson);
+            showTextBottomSheet(
+                    "APACHE",
+                    R.string.license_gson,
+                    R.string.url_gson
+            );
+        } else if(v.getId() == R.id.linear_license_xzing_android) {
+            IconUtil.start(activity, R.id.image_license_xzing_android);
+            showTextBottomSheet(
+                    "APACHE",
+                    R.string.license_xzing_android,
+                    R.string.url_zxing_android
+            );
         }
     }
 
