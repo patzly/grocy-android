a3ae4bed0548d88bbf538c1fbcb686dabb9755be
patzly <patrick.zedler.music@gmail.com>
2021-04-26 00:24:19 +0200

    Fix view binding issue with copyright notice in xml

--- app/src/main/java/xyz/zedler/patrick/grocy/bottomappbar/BottomAppBar.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/bottomappbar/BottomAppBar.java
@@ -14,23 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Grocy Android. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (c) 2020-2021 by Patrick Zedler & Dominic Zedler
- */
-
-/*
- * Copyright 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) 2020-2021 by Patrick Zedler and Dominic Zedler
  */
 
 package xyz.zedler.patrick.grocy.bottomappbar;
