67476c700805cadb63359930644d2af00921d623
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2022-02-21 09:25:33 +0100

    Improvements for Home Assistant (#249)

--- app/src/main/java/xyz/zedler/patrick/grocy/fragment/LoginApiQrCodeFragment.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/fragment/LoginApiQrCodeFragment.java
@@ -42,9 +42,14 @@ import xyz.zedler.patrick.grocy.util.NetUtil;
 
 public class LoginApiQrCodeFragment extends BaseFragment implements BarcodeListener {
 
+  private static final int SCAN_GROCY_KEY = 0;
+  private static final int SCAN_HASS_TOKEN = 1;
+
   private FragmentLoginApiQrCodeBinding binding;
   private MainActivity activity;
+  private LoginApiQrCodeFragmentArgs args;
   private EmbeddedFragmentScanner embeddedFragmentScanner;
+  private int pageStatus = SCAN_GROCY_KEY;
 
   @Override
   public View onCreateView(
@@ -73,6 +78,9 @@ public class LoginApiQrCodeFragment extends BaseFragment implements BarcodeListe
   @Override
   public void onViewCreated(@Nullable View view, @Nullable Bundle savedInstanceState) {
     activity = (MainActivity) requireActivity();
+    args = LoginApiQrCodeFragmentArgs.fromBundle(requireArguments());
+    pageStatus = args.getGrocyApiKey() == null ? SCAN_GROCY_KEY : SCAN_HASS_TOKEN;
+
     binding.setFragment(this);
     binding.setClickUtil(new ClickUtil());
     embeddedFragmentScanner.setScannerVisibilityLive(new MutableLiveData<>(true));
@@ -98,27 +106,42 @@ public class LoginApiQrCodeFragment extends BaseFragment implements BarcodeListe
 
   @Override
   public void onBarcodeRecognized(String rawValue) {
-    String[] resultSplit = rawValue.split("\\|");
-    if (resultSplit.length != 2) {
-      activity.showMessage(R.string.error_api_qr_code);
-      embeddedFragmentScanner.startScannerIfVisible();
-      return;
-    }
-    String apiURL = resultSplit[0];
-    String serverURL = apiURL.replaceAll("/api$", "");
-    String ingressProxyId = null;
-    if (serverURL.startsWith("/api/hassio_ingress/")) {
-      ingressProxyId = serverURL.replace("/api/hassio_ingress/", "");
-    }
-    String apiKey = resultSplit[1];
-    if (ingressProxyId == null) {
-      navigate(LoginApiQrCodeFragmentDirections
-          .actionLoginApiQrCodeFragmentToLoginRequestFragment(serverURL, apiKey));
-    } else { // grocy home assistant add-on used
+    if(pageStatus == SCAN_GROCY_KEY) {
+      String[] resultSplit = rawValue.split("\\|");
+      if (resultSplit.length != 2) {
+        activity.showMessage(R.string.error_api_qr_code);
+        embeddedFragmentScanner.startScannerIfVisible();
+        return;
+      }
+      String apiURL = resultSplit[0];
+      String serverURL = apiURL.replaceAll("/api$", "");
+      String ingressProxyId = null;
+      if (serverURL.startsWith("/api/hassio_ingress/")) {
+        ingressProxyId = serverURL.replace("/api/hassio_ingress/", "");
+      }
+      String apiKey = resultSplit[1];
+
+      if (ingressProxyId == null) {
+        navigate(LoginApiQrCodeFragmentDirections
+            .actionLoginApiQrCodeFragmentToLoginRequestFragment(serverURL, apiKey));
+      } else { // grocy home assistant add-on used
+        navigate(LoginApiQrCodeFragmentDirections
+            .actionLoginApiQrCodeFragmentSelf()
+            .setGrocyIngressProxyId(ingressProxyId)
+            .setGrocyApiKey(apiKey));
+      }
+    } else if (pageStatus == SCAN_HASS_TOKEN) {
+      String[] resultSplit = rawValue.split("\\.");
+      if (resultSplit.length != 3) {
+        activity.showMessage(R.string.error_token_qr_code);
+        embeddedFragmentScanner.startScannerIfVisible();
+        return;
+      }
       navigate(LoginApiQrCodeFragmentDirections
           .actionLoginApiQrCodeFragmentToLoginApiFormFragment()
-          .setGrocyIngressProxyId(ingressProxyId)
-          .setGrocyApiKey(apiKey));
+          .setGrocyIngressProxyId(args.getGrocyIngressProxyId())
+          .setGrocyApiKey(args.getGrocyApiKey())
+          .setHomeAssistantToken(rawValue));
     }
   }
 
@@ -144,6 +167,10 @@ public class LoginApiQrCodeFragment extends BaseFragment implements BarcodeListe
 
   public void showFeedbackBottomSheet() {
     activity.showBottomSheet(new FeedbackBottomSheet());
+  }
+
+  public boolean isPageForGrocyKey() {
+    return pageStatus == SCAN_GROCY_KEY;
   }
 
   @Override
