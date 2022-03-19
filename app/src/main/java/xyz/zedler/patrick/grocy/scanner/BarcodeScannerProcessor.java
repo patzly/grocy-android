9405232b4b17d2d30fba674446e4287954ef9e38
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2022-03-19 18:49:52 +0100

    Update libs

--- app/src/main/java/xyz/zedler/patrick/grocy/scanner/BarcodeScannerProcessor.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/scanner/BarcodeScannerProcessor.java
@@ -27,10 +27,10 @@ import android.graphics.RectF;
 import android.util.Log;
 import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.Task;
-import com.google.mlkit.vision.barcode.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.common.InputImage;
 import java.util.List;
 
