ea5b127f85038ad33438665555725bfa86707931
Dominic Zedler <mrjohnzoidberg@mailbox.org>
2022-09-12 16:00:22 +0200

    Improvements for editor and added text loading

--- app/src/main/java/xyz/zedler/patrick/grocy/view/Summernote.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/view/Summernote.java
@@ -1,5 +1,6 @@
 package xyz.zedler.patrick.grocy.view;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -11,56 +12,62 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import xyz.zedler.patrick.grocy.util.UnitUtil;
 
 /**
  * Created by Avinash on 01-04-2016.
  */
 public class Summernote extends WebView  {
-    String text="";
+    String text = "";
     private int REQUEST_FILE_PICKER = 1;
     private ValueCallback<Uri> mFilePathCallback4;
     private ValueCallback<Uri[]> mFilePathCallback5;
     Context context;
+
     public Summernote(Context context) {
         super(context);
-        this.context =context;
+        this.context = context;
         enable_summernote();
     }
+
     public Summernote(Context context, AttributeSet attrs){
         super(context, attrs);
-        this.context =context;
+        this.context = context;
         enable_summernote();
     }
+
     public Summernote(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        this.context =context;
+        this.context = context;
         enable_summernote();
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
     public void enable_summernote(){
         this.getSettings().setJavaScriptEnabled(true);
         this.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        //this.getSettings().setBuiltInZoomControls(true);
         this.addJavascriptInterface(new MyJavaScriptInterface(), "android");
-        this. getSettings().setLoadWithOverviewMode(true);
-        this. getSettings().setUseWideViewPort(true);
+        this.getSettings().setLoadWithOverviewMode(true);
+        this.getSettings().setUseWideViewPort(true);
         this.getSettings().setAllowFileAccessFromFileURLs(true);
         this.getSettings().setAllowUniversalAccessFromFileURLs(true);
-        this.loadUrl("file:///android_asset/summernote.html");
+        this.loadUrl("file:///android_asset/summernote/summernote.html");
+
         setWebChromeClient(new WebChromeClient() {
             public void openFileChooser(ValueCallback<Uri> filePathCallback) {
                 mFilePathCallback4 = filePathCallback;
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("image/*");
-                ((Activity)context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
+                ((Activity) context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
             }
 
-            public void openFileChooser(ValueCallback filePathCallback, String acceptType) {
+            public void openFileChooser(ValueCallback<Uri> filePathCallback, String acceptType) {
                 mFilePathCallback4 = filePathCallback;
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("image/*");
-                ((Activity)context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
+                ((Activity) context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
             }
 
             public void openFileChooser(ValueCallback<Uri> filePathCallback, String acceptType, String capture) {
@@ -68,7 +75,7 @@ public class Summernote extends WebView  {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("image/*");
-                ((Activity)context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
+                ((Activity) context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
             }
 
             @Override
@@ -77,17 +84,19 @@ public class Summernote extends WebView  {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("image/*");
-                ((Activity)context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
+                ((Activity) context).startActivityForResult(Intent.createChooser(intent, "File Chooser"), REQUEST_FILE_PICKER);
                 return true;
             }
         });
     }
+
     class MyJavaScriptInterface {
         @JavascriptInterface
         public void getText(String html) {
-            text=html;
+            text = html;
         }
     }
+
     public void setText(final String html) {
         setWebViewClient(new WebViewClient() {
             @Override
@@ -95,51 +104,51 @@ public class Summernote extends WebView  {
                 setText(html);
             }
         });
+        this.loadUrl("javascript:(function f() {"
+            + "document.getElementsByClassName('note-toolbar')[0].style.padding = '11px 16px 16px 16px';"
+            + "})()");
+        this.loadUrl("javascript:(function f() {"
+            + "document.getElementsByClassName('note-editable')[0].style.padding = '16px 16px 120px 16px';"
+            + "})()");
         this.loadUrl("javascript:$('#summernote').summernote('reset');");
         this.loadUrl("javascript:$('#summernote').summernote('code', '" + html.replace("'","\\'") + "');");
+
     }
     public String getText() {
         text = "P/%TE5XpkAijBc%LjA;_-pZcbiU25E6feX5y/n6qxCTmhprLrqC3H%^hU!%q2,k'm`SHheoW^'mQ~zW93,C?~GtYk!wi/&'3KxW8";
         this.loadUrl("javascript:window.android.getText" + "(document.getElementsByClassName('note-editable')[0].innerHTML);");
-        int i=0;
         try{
-            while(text.equals("P/%TE5XpkAijBc%LjA;_-pZcbiU25E6feX5y/n6qxCTmhprLrqC3H%^hU!%q2,k'm`SHheoW^'mQ~zW93,C?~GtYk!wi/&'3KxW8")&&i<100){
+            for (int i=0; i<100; i++) {
+                if (!text.equals("P/%TE5XpkAijBc%LjA;_-pZcbiU25E6feX5y/n6qxCTmhprLrqC3H%^hU!%q2,k'm`SHheoW^'mQ~zW93,C?~GtYk!wi/&'3KxW8")) {
+                    break;
+                }
                 Thread.sleep(200);
-                i++;
             }
-        }catch (Exception e ){text = "Unable get the Text";}
+        } catch (Exception e) {
+            text = "Unable get the Text";
+        }
         return text;
     }
-    public void setRequestCodeforFilepicker(int i){
+    public void setRequestCodeforFilepicker(int i) {
         REQUEST_FILE_PICKER=i;
     }
-    public void onActivityResult(int requestCode, int resultCode, Intent intent)
-    {
-        if(requestCode==REQUEST_FILE_PICKER)
-        {
-            if(mFilePathCallback4!=null)
-            {
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(requestCode==REQUEST_FILE_PICKER) {
+            if(mFilePathCallback4!=null) {
                 Uri result = intent==null || resultCode!=Activity.RESULT_OK ? null : intent.getData();
-                if(result!=null)
-                {
+                if (result!=null) {
                     Uri path = intent.getData();
                     mFilePathCallback4.onReceiveValue(path);
-                }
-                else
-                {
+                } else {
                     mFilePathCallback4.onReceiveValue(null);
                 }
             }
-            if(mFilePathCallback5!=null)
-            {
+            if(mFilePathCallback5!=null) {
                 Uri result = intent==null || resultCode!=Activity.RESULT_OK ? null : intent.getData();
-                if(result!=null)
-                {
+                if(result!=null) {
                     Uri path = intent.getData();
                     mFilePathCallback5.onReceiveValue(new Uri[]{path});
-                }
-                else
-                {
+                } else {
                     mFilePathCallback5.onReceiveValue(null);
                 }
             }
